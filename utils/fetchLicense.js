280efd4b8e53ebfd720fb25b26ecf968e43cf6dd
issammani <issamouu69@gmail.com>
2020-06-02 20:42:29 +0000

    Use an async generator

--- utils/fetchLicense.js
+++ utils/fetchLicense.js
@@ -1,13 +1,11 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
-const JSONStream = require('JSONStream');
 const canonicalUrl = require('./canonicalUrl');
 
 const source_url = 'https://opensource.org/licenses/alphabetical';
 const licensesListSelector = '.field-items ul li a';
 const licenseContentSelector = '#main-content';
 
-let licenseCount = 0;
  
 const fetchLicenseContent = async (licenseUrl) => {
     const html_doc = await axios.get(licenseUrl);
@@ -17,33 +15,28 @@ const fetchLicenseContent = async (licenseUrl) => {
     return content;
 };
 
-const fetchLicenseList = async (writeStream) => {
-    const jsonWriter = JSONStream.stringify();
-    jsonWriter.pipe(writeStream);
-
+const fetchLicenseList = async function* () {
+    // Fetch license list
     const html_doc = await axios.get(source_url);
     const $ = await cheerio.load(html_doc.data);
-    const licensesList = $(licensesListSelector);
+    const licensesList = await $(licensesListSelector);
 
-    licenseCount = licensesList.length;
 
-    licensesList.each(async (index, element) => {
-        const content = await fetchLicenseContent(canonicalUrl($(element).attr('href')));
-        const nameParts = $(element).text().split(/\((.*)\)/);
-        const chunk = {
+    for (let license  of licensesList.toArray()){
+        const url = canonicalUrl(license.attribs.href);
+        const content = await fetchLicenseContent(url);
+        const nameParts = license.children[0].data.split(/\((.*)\)/); // Assumes first child is a text node
+        
+        const _license = {
             name: nameParts[0],
             shortId: nameParts.length >= 2 ? nameParts[1] : nameParts[0],
-            url: canonicalUrl($(element).attr('href')),
+            url: url,
             content: content.trim().replace(/ *\n */g,'\n').replace(/(\r? ?\n ?|\r ?)+/g,'$1')
         };
-        jsonWriter.write(chunk);
 
-        licenseCount--;
-        
-        if(licenseCount === 0){
-            jsonWriter.end();
-        }
-    });
+        yield _license;
+    }
+
 };
 
 module.exports = fetchLicenseList;
