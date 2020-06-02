436c7aff371c322b179325f57143aa977e1cf172
issammani <issamouu69@gmail.com>
2020-06-02 23:22:23 +0000

    Fix language_injector issues
    - Empty snippets array before insertion.
    
    - Fix wrong path issue.

--- utils/language_inject.js
+++ utils/language_inject.js
@@ -4,11 +4,12 @@ const languages_ids = require('./language_ids');
 fs.readFile(`${__dirname}/../package.json`, (err, data) => {
     if (err) throw err;
     let package = JSON.parse(data);
-    let snippets = package.contributes.snippets;
-    
+    // Empty snippets array
+    package.contributes.snippets = [];
+
     // Add support for languages in languages_ids
     languages_ids
-        .forEach(language_id => snippets.push( {language: language_id, path: '../snippets/licenses.code-snippets'} ));
+        .forEach(language_id => package.contributes.snippets.push( {language: language_id, path: './snippets/licenses.code-snippets'} ));
     
     // Overwrite package.json
     fs.writeFileSync(`${__dirname}/../package.json`, JSON.stringify(package,null, 4));
